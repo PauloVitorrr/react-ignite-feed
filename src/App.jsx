7535aae78a4af0ee79f73829d1040cacebb5d865
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-06-17 21:48:56 -0300

    feat: iniciando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import { Header } from './components/Header.jsx'
 
 import './global.css'
 
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar.jsx'
+
 export default function App() {
 
   return (
     <div>
       <Header/>
-      <h1>Hello World!</h1>  
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, quis!"
-      />
-      <Post
-        author="Paulo Vitor"
-        content="UM autor muito legal!!!!!"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+          author="Diego Fernandes"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, quis!"
+        />
+        <Post
+          author="Paulo Vitor"
+          content="UM autor muito legal!!!!!"
+        />
+        </main>
+      </div>
     </div>
   )
 }
