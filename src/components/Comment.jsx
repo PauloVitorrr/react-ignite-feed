c534bf265b54f5b85bc352b81253ca6db46113bd
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-06-21 21:03:05 -0300

    feat: componente comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import styles from './Comment.module.css'
+
+import { Avatar } from './Avatar.jsx'
+import { ThumbsDown, Trash } from 'phosphor-react'
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <Avatar/>
+
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                        <strong>Paulo Vitor Sousa</strong>
+                        <time title="19 de Junho ás 08:15h" dateTime="2023-06-19">Cerca de 1 hora atrás</time>
+                    </div>
+
+                    <button title="Deletar Comentário">
+                        <Trash size={20}/>
+                    </button>
+                </header>
+                <p>Muito bom Devon, parabéns!! 👏👏</p>
+            </div>
+
+            <footer>
+                <button>
+                    <ThumbsDown />
+                    Aplaudir <span>20</span>
+                </button>
+            </footer>
+        </div>
+
+
+
+
+        </div>
+    )
+}
